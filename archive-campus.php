bf90ffbc3b2f825b200b7b99877e4c9df1c4cf16
Kerry Ruddock <kerry.ruddock@hotmail.com>
2018-05-24 19:02:24 -0700

    lesson 12.47 created single-campus.php

--- archive-campus.php
+++ archive-campus.php
@@ -9,20 +9,22 @@ pageBanner(array(
 
 <div class="container container--narrow page-section">
 
-<div class="acf-map">
+  <div class="acf-map">
 
-<?php
-  while(have_posts()) {
-    the_post();
-    $mapLocation = get_field('map_location');
-   ?>
-    <div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng']; ?>"></div>
-  <?php }
-  echo paginate_links();
-?>
-</div>
-
-
+  <?php
+    while(have_posts()) {
+      the_post();
+      $mapLocation = get_field('map_location');
+     ?>
+      <div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php
+        echo $mapLocation['lng']; ?>">
+        <h3><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+        <?php echo $mapLocation['address']; ?>
+      </div>
+    <?php }
+    echo paginate_links();
+  ?>
+  </div>
 
 </div>
 
