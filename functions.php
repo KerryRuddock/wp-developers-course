1808932f6244d51905dee984dbca041b44016a50
Kerry Ruddock <kerry.ruddock@hotmail.com>
2018-06-11 14:28:07 -0700

    Lesson 14.57 Continue mods with our Search

--- functions.php
+++ functions.php
@@ -28,12 +28,17 @@ function pageBanner($args = NULL) {
   </div>
   <?php  
 }
+
+// load CSS and javascript files
 function university_files() {
   wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyAscX0N2UhDyc166yI8HgIS6Pmg0zG_9T4', NULL, 1.0, true );   
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true );
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+  wp_localize_script('main-university-js', 'universityData', array(
+    'root_url' =>  get_site_url()
+  ));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -13614,6 +13614,7 @@ function () {
   function Search() {
     _classCallCheck(this, Search);
 
+    this.addSearchHTML();
     this.resultsDiv = (0, _jquery.default)("#search-overlay__results");
     this.openButton = (0, _jquery.default)(".js-search-trigger");
     this.closeButton = (0, _jquery.default)(".search-overlay__close");
@@ -13676,18 +13677,28 @@ function () {
     value: function getResults() {
       var _this = this;
 
-      _jquery.default.getJSON('http://wp-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
-        _this.resultsDiv.html("\n        <h2 class=\"search-overlay__section-title\">General Information</h2>\n        <ul class=\"link-list min-list\">\n          ".concat(posts.map(function (item) {
+      // http GET search request, on success post(s) title(s) is/are displayed
+      _jquery.default.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
+        _this.resultsDiv.html("\n        <h2 class=\"search-overlay__section-title\">General Information</h2>\n        ".concat(posts.length ? '<ul class="link-list min-list">' : '<p>No Search Results found</p>', "\n          ").concat(posts.map(function (item) {
           return "<li><a href=\"".concat(item.link, "\">").concat(item.title.rendered, "</a></li>");
-        }).join(''), "\n        </ul>\n      "));
+        }).join(''), "\n        ").concat(posts.length ? '</ul>' : '', " \n      "));
+
+        _this.spinnerActive = false;
       });
     }
   }, {
     key: "openOverlay",
     value: function openOverlay() {
+      var _this2 = this;
+
       this.searchActive = true;
       this.searchOverlay.addClass("search-overlay--active");
       (0, _jquery.default)("body").addClass("body-no-scroll");
+      this.searchField.val('');
+      this.resultsDiv.html('');
+      setTimeout(function () {
+        return _this2.searchField.focus();
+      }, 301);
     }
   }, {
     key: "closeOverlay",
@@ -13695,6 +13706,11 @@ function () {
       this.searchActive = false;
       this.searchOverlay.removeClass("search-overlay--active");
       (0, _jquery.default)("body").removeClass("body-no-scroll");
+    }
+  }, {
+    key: "addSearchHTML",
+    value: function addSearchHTML() {
+      (0, _jquery.default)("body").append("\n      <div class=\"search-overlay\">\n        <div class=\"search-overlay__top\">\n          <div class=\"container\">\n            <i class=\"fa fa-search search-overlay__icon\" aria-hidden=\"true\"></i>\n            <input type=\"text\" class=\"search-term\" placeholder=\"Search for?\" id=\"js-search-term\">\n            <i class=\"fa fa-window-close search-overlay__close\" aria-hidden=\"true\"></i>\n          </div>\n        </div>\n        \n        <div class=\"container\">\n          <div id=\"search-overlay__results\">\n          </div>\n        </div>\n      </div>\n    ");
     }
   }]);
 
