18592db948f6e4885b4178c002bd47934b7c4a88
Kerry Ruddock <kerry.ruddock@hotmail.com>
2018-06-20 15:35:25 -0700

    15.62 Create your own raw json data

--- inc/search-route.php
+++ inc/search-route.php
@@ -12,6 +12,19 @@ function universityRegisterSearch() {
 add_action('rest_api_init', 'universityRegisterSearch');
 
 function universitySearchResults() {
-  return 'Congrats, you created a route.';
+  $professors = new WP_Query(array(
+    'post_type' => 'professor'
+  ));
+  $professorResults = array();
+  
+  while ( $professors->have_posts() ) {
+    $professors->the_post();
+    array_push( $professorResults, array(
+      'title' => get_the_title(),
+      'permalink' => get_the_permalink()
+    ));
+  }
+  
+  return $professorResults;
 }
 
